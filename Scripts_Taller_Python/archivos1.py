f3ae56b70f06854960d8bb536fab3b7aff2bef5a
Juanezm <juanezm73@gmail.com>
2017-04-05 19:28:48 +0200

    test

--- Scripts_Taller_Python/archivos1.py
+++ Scripts_Taller_Python/archivos1.py
@@ -2,18 +2,18 @@
 """
 Created on Wed Mar 01 16:26:00 2017
 
-I Curso de Introducción a Python Orientado a la Resolución 
+I Curso de Introducción a Python Orientado a la Resolución
 de Problemas de Optimización en Ingeniería
 
 Archivos 1:
-    
+
 Script de introducción al módulo os
 
-@authors: DanyGR MarioDM IgnacioGP
+@authors: DanyGR MarioDM IgnacioGP .
 """
 
 import os
-    
+
 print "directorio actual", os.getcwd()
 
 dir_actual = os.getcwd()
@@ -40,5 +40,5 @@ for root, dirs, files in os.walk(".", topdown=False):
         print(os.path.join(root, name)) # os.path.join sirve para concatenar los path
     for name in dirs:
         print(os.path.join(root, name))
-        
+
 # ejercicio crear subdirectorios y comprobar el resultado de la celda
